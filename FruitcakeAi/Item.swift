97adaa5131a9a829306cbd820bfbd393d0a1ce2e
Wombolz <Wombolz@users.noreply.github.com>
2026-02-28 17:57:29 -0500

    Initial Commit

---
+++ FruitcakeAi/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  FruitcakeAi
+//
+//  Created by jwomble on 2/28/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ FruitcakeAiTests/FruitcakeAiTests.swift
@@ -0,0 +1,16 @@
+//
+//  FruitcakeAiTests.swift
+//  FruitcakeAiTests
+//
+//  Created by jwomble on 2/28/26.
+//
+
+import Testing
+
+struct FruitcakeAiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
